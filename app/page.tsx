ab43efcffc4930ede4f834111d085b943f704b51
ThisAbhinav <this.abhinav26@gmail.com>
2023-10-26 02:20:56 +0530

    added button

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center ">
-      <Link href="/linear_regression_workshop">Linear Regression Workshop</Link>
-      <Link href="/ml_bytes">ML Bytes</Link>
+    <div className="flex flex-col justify-center items-center gap-5">
+      {/* <Link href="/linear_regression_workshop">Linear Regression Workshop</Link> */}
+      <Link href="https://colab.research.google.com/drive/16G5_NW3W9FFkuDjDz1MTT1Iy6ngV_Cds?usp=sharing">
+        <Button className="bg-main_blue1 hover:bg-main_blued">
+          Download the Notebook
+        </Button>
+      </Link>
+      <Link href="/ml_bytes" className="underline">
+        ML Bytes
+      </Link>
 
-      <Link href="/upcoming_events">Upcoming Events</Link>
+      <Link href="/upcoming_events" className="underline">
+        Upcoming Events
+      </Link>
 
-      <Link href="/team">Team</Link>
+      <Link href="/team" className="underline">
+        Team
+      </Link>
     </div>
   );
 }

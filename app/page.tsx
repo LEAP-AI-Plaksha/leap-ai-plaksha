67a938995cb9adbad818790bf21310e102f7e301
ThisAbhinav <this.abhinav26@gmail.com>
2023-10-28 16:33:00 +0530

    asdasd

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center gap-5">
-      {/* <Link href="/linear_regression_workshop">Linear Regression Workshop</Link> */}
-      <Link href="https://colab.research.google.com/drive/16G5_NW3W9FFkuDjDz1MTT1Iy6ngV_Cds?usp=sharing">
+      <Link href="https://colab.research.google.com/drive/1i44Y2rmeQT0R9FAJy2jORyD2WEA7b1A5?usp=sharing">
         <Button className="bg-main_blue1 hover:bg-main_blued">
           Download the Notebook
         </Button>

0c897ce873549fec5d373295202703a546c9eb6e
ThisAbhinav <this.abhinav26@gmail.com>
2023-10-28 16:22:29 +0530

    added new link

--- app/linear_regression_workshop/page.tsx
+++ app/linear_regression_workshop/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Page() {
   return (
     <div className="flex flex-col justify-center items-center ">
-      <Link href="https://colab.research.google.com/drive/16G5_NW3W9FFkuDjDz1MTT1Iy6ngV_Cds?usp=sharing">
+      <Link href="https://colab.research.google.com/drive/1i44Y2rmeQT0R9FAJy2jORyD2WEA7b1A5?usp=sharing">
         <Button className="bg-main_blue1 hover:bg-main_blued">
           Download the Notebook
         </Button>

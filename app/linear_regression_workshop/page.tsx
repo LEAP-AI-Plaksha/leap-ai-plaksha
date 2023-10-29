445cd968ff05ac31c3584ccab2a9fdd175cb3034
ThisAbhinav <this.abhinav26@gmail.com>
2023-10-29 20:42:45 +0530

    added linear workshop page

--- app/linear_regression_workshop/page.tsx
+++ app/linear_regression_workshop/page.tsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
-    <div className="flex flex-col justify-center items-center ">
-      <Link href="https://colab.research.google.com/drive/1i44Y2rmeQT0R9FAJy2jORyD2WEA7b1A5?usp=sharing">
-        <Button className="bg-main_blue1 hover:bg-main_blued">
-          Download the Notebook
-        </Button>
+    <div className="flex flex-col justify-center items-center gap-5">
+      <Link
+        href="https://colab.research.google.com/drive/1i44Y2rmeQT0R9FAJy2jORyD2WEA7b1A5?usp=sharing"
+        className="underline"
+      >
+        Get the Notebook
+      </Link>
+      <Link
+        href="/presentations/Linear_Regression_Workshop_Presentation.pdf"
+        className="underline"
+        target="_blank"
+      >
+        Download the Presentation
+      </Link>
+      <Link
+        href="https://plakshauniversity1-my.sharepoint.com/:w:/g/personal/abhinav_lodha_plaksha_edu_in/ERUsW6shsJxHk9GpKc6pUcABRUj7sD_8fnvk9Mku-jKJRQ?rtime=oNQi6ofY20g"
+        className="underline"
+      >
+        Post Workshop Content
       </Link>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center gap-5">
-      <Link href="https://colab.research.google.com/drive/1i44Y2rmeQT0R9FAJy2jORyD2WEA7b1A5?usp=sharing">
-        <Button className="bg-main_blue1 hover:bg-main_blued">
-          Download the Notebook
-        </Button>
+      <Link href="/linear_regression_workshop" className="underline">
+        Linear Regression Workshop
       </Link>
       <Link href="/ml_bytes" className="underline">
         ML Bytes

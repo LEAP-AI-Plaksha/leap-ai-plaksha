1f1d020d84b207e50bdae47e55a8de688ea50a4e
ThisAbhinav <this.abhinav26@gmail.com>
2023-10-30 11:14:15 +0530

    added some margins

--- app/linear_regression_workshop/page.tsx
+++ app/linear_regression_workshop/page.tsx
@@ -5,20 +5,20 @@ export default function Page() {
     <div className="flex flex-col justify-center items-center gap-5">
       <Link
         href="https://colab.research.google.com/drive/1i44Y2rmeQT0R9FAJy2jORyD2WEA7b1A5?usp=sharing"
-        className="underline"
+        className="underline text-sm md:text-xl text-center"
       >
         Get the Notebook
       </Link>
       <Link
         href="/presentations/Linear_Regression_Workshop_Presentation.pdf"
-        className="underline"
+        className="underline text-sm md:text-xl text-center"
         target="_blank"
       >
         Download the Presentation
       </Link>
       <Link
         href="https://plakshauniversity1-my.sharepoint.com/:w:/g/personal/abhinav_lodha_plaksha_edu_in/ERUsW6shsJxHk9GpKc6pUcABRUj7sD_8fnvk9Mku-jKJRQ?rtime=oNQi6ofY20g"
-        className="underline"
+        className="underline text-sm md:text-xl text-center"
       >
         Post Workshop Content
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,27 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center gap-5">
-      <Link href="/linear_regression_workshop" className="underline">
+      <Link
+        href="/linear_regression_workshop"
+        className="underline text-sm md:text-xl text-center"
+      >
         Linear Regression Workshop
       </Link>
-      <Link href="/ml_bytes" className="underline">
+      <Link
+        href="/ml_bytes"
+        className="underline text-sm md:text-xl text-center"
+      >
         ML Bytes
       </Link>
 
-      <Link href="/upcoming_events" className="underline">
+      <Link
+        href="/upcoming_events"
+        className="underline text-sm md:text-xl text-center"
+      >
         Upcoming Events
       </Link>
 
-      <Link href="/team" className="underline">
+      <Link href="/team" className="underline text-sm md:text-xl text-center">
         Team
       </Link>
     </div>

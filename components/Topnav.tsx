d8d708137ddb6795dad55077a045774c7dbb6cbd
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2024-02-16 23:09:53 +0530

    add exciting new website

--- components/Topnav.tsx
+++ components/Topnav.tsx
@@ -3,10 +3,12 @@ import leap_logo from "@/media/images/leap_logo.svg";
 import Link from "next/link";
 const TopNav = () => {
   return (
-    <div className="p-3">
+    <div className="p-3 flex justify-between items-stretch">
       <Link href="/">
         <Image src={leap_logo} width={100} alt="Picture of the author" />
       </Link>
+
+      <div className="self-center mr-6">Exciting new website coming soon!</div>
     </div>
   );
 };

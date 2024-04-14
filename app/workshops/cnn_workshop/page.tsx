86f9a8b04063232bed56108a206d8dfae2e31365
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2024-04-14 08:58:22 +0530

    add the day2 nb

--- app/workshops/cnn_workshop/page.tsx
+++ app/workshops/cnn_workshop/page.tsx
@@ -7,8 +7,27 @@ export default function Page() {
         href="https://colab.research.google.com/drive/1oIAQ8Pa66xecsaLwONHDQrtbal-bdY_n?usp=sharing"
         className="underline text-sm md:text-xl text-center"
       >
-        Get the Notebook
+        Get the Day 1 Notebook
       </Link>
+      <Link
+        href="https://colab.research.google.com/drive/1TFmJ-aGHkHC61cOTYZD0q9RhL1tMmoe2?usp=sharing"
+        className="underline text-sm md:text-xl text-center"
+      >
+        Get the Day 2 Notebook
+      </Link>
+      <Link
+        href="https://plakshauniversity1-my.sharepoint.com/:b:/g/personal/arnav_rustagi_plaksha_edu_in/EV9AdXzlFA9AnrnTGqzXiVQBSHKoEW34WNNUqlLKdx1p7Q?e=dGwHvA"
+        className="underline text-sm md:text-xl text-center"
+      >
+        Get the Post-Workshop Content
+      </Link>
+
+      {/* <Link
+        href="https://colab.research.google.com/drive/1oIAQ8Pa66xecsaLwONHDQrtbal-bdY_n?usp=sharing"
+        className="underline text-sm md:text-xl text-center"
+      >
+        Get the Presentation
+      </Link> */}
     </div>
   );
 }

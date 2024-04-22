bb3ec76c6958267aa4ee1bfa9492804b76d14917
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2024-04-22 11:00:43 +0530

    add links for CNN post content

--- app/workshops/cnn_workshop/page.tsx
+++ app/workshops/cnn_workshop/page.tsx
@@ -6,28 +6,51 @@ export default function Page() {
       <Link
         href="https://colab.research.google.com/drive/1oIAQ8Pa66xecsaLwONHDQrtbal-bdY_n?usp=sharing"
         className="underline text-sm md:text-xl text-center"
+        target="_blank"
       >
         Get the Day 1 Notebook
       </Link>
       <Link
         href="https://colab.research.google.com/drive/1TFmJ-aGHkHC61cOTYZD0q9RhL1tMmoe2?usp=sharing"
         className="underline text-sm md:text-xl text-center"
+        target="_blank"
       >
         Get the Day 2 Notebook
       </Link>
+      
+      <br/>
+
       <Link
         href="https://plakshauniversity1-my.sharepoint.com/:b:/g/personal/arnav_rustagi_plaksha_edu_in/EV9AdXzlFA9AnrnTGqzXiVQBSHKoEW34WNNUqlLKdx1p7Q?e=dGwHvA"
         className="underline text-sm md:text-xl text-center"
+        target="_blank"
       >
         Get the Post-Workshop Content
       </Link>
 
-      {/* <Link
-        href="https://colab.research.google.com/drive/1oIAQ8Pa66xecsaLwONHDQrtbal-bdY_n?usp=sharing"
+      <Link
+        href="https://colab.research.google.com/drive/1Sc8z5SQ4_VHSNozLcC8lJQL_cfXviz6R?usp=sharing"
         className="underline text-sm md:text-xl text-center"
+        target="_blank"
+      >
+        Get Day 1 Completed Notebook
+      </Link>
+
+      <Link
+        href="https://colab.research.google.com/drive/1Qjw7Wh2WoENYAECSvZX9DJQthrR7s4l2?usp=sharing"
+        className="underline text-sm md:text-xl text-center"
+        target="_blank"
+      >
+        Get Day 2 Completed Notebook
+      </Link>
+
+      <Link
+        href="/presentations/LEAP_CNNWorkshop.pdf"
+        className="underline text-sm md:text-xl text-center"
+        target="_blank"
       >
         Get the Presentation
-      </Link> */}
+      </Link>
     </div>
   );
 }
